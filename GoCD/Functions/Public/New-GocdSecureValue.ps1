e12dee99a7b19b5d022ebabd4a2b7fea4bb4bc9c
Nicholas Dille <nicholasdille@users.noreply.github.com>
2017-11-27 14:56:05 +0100

    Fixed due to script analyzer

--- GoCD/Functions/Public/New-GocdSecureValue.ps1
+++ GoCD/Functions/Public/New-GocdSecureValue.ps1
@@ -1,4 +1,9 @@
 ﻿function New-GocdSecureValue {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+        "PSUseShouldProcessForStateChangingFunctions", 
+        "", 
+        Justification = "Creates in-memory object only."
+    )]
     [CmdletBinding()]
     param(
         [Parameter(Mandatory)]
