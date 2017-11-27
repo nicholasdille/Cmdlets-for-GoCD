849ea7c8f2513d05ad19a9b5ab7b598092f37a0c
Nicholas Dille <nicholasdille@users.noreply.github.com>
2017-11-27 14:47:14 +0100

    Fixed due to upstream change

--- GoCD/Functions/Public/Get-GocdUser.ps1
+++ GoCD/Functions/Public/Get-GocdUser.ps1
@@ -18,7 +18,7 @@
 
     $Response = Invoke-GocdApi -Path "/api/users" -Accept 'application/vnd.go.cd.v1+json'
     $ContentBytes = $Response | Select-Object -ExpandProperty Content
-    $Users = ConvertFrom-Bytes -Data $ContentBytes -Encoding ASCII | ConvertFrom-Json
+    $Users = ConvertFrom-ByteArray -Data $ContentBytes -Encoding ASCII | ConvertFrom-Json
     $Users = $Users._embedded.users | Where-Object { $_.display_name -like $UserName -and $_.login_name -like $Login }
 
     if ($Raw) {
