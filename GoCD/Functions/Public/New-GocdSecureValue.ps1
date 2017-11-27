c5c966337bed0e44b337e526602166d2a794d5f1
Nicholas Dille <nicholasdille@users.noreply.github.com>
2017-11-27 14:47:28 +0100

    Added encryption for secure variables

---
+++ GoCD/Functions/Public/New-GocdSecureValue.ps1
@@ -0,0 +1,13 @@
+﻿function New-GocdSecureValue {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Value
+    )
+
+    $Data = '{"value":"' + $Value + '"}'
+    $Response = Invoke-GocdApi -Path '/api/admin/encrypt' -Accept 'application/vnd.go.cd.v1+json' -Headers @{'Content-Type' = 'application/json'} -Body $Data -Method Post
+    $Response | ConvertFrom-Json | Select-Object -ExpandProperty encrypted_value
+}
